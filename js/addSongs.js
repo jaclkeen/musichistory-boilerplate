af68bb50f5b534dfb15a2ccd10a7221d79f59368
Jacob Keen <jaclkeen@gmail.com>
2016-08-21 13:55:07 -0500

    ready for browserify

---
+++ js/addSongs.js
@@ -0,0 +1,108 @@
+function addToDom(song_info){
+  song_info.forEach(function(item){
+    // FUNCTION CALL THAT PRINTS JSON DATA TO DOM
+    printStuff(item)
+    // ADDS JSON DATA TO PRIVATE _SONGS ARRAY
+    Songs.addSong(item)
+  });
+// ADDS ARTIST TO FILTER TAB
+addArtistsIntoFilter(song_info)
+// ADDS ALBUM TO FILTER TAB
+addAlbumsIntoFilter(song_info)
+}
+
+function addUserInputSong(){
+  var $add_song = $("#add_song").val();
+  var $add_album = $("#add_album").val();
+  var $add_artist = $("#add_artist").val();
+  var $add_genre = $("#add_genre").val();
+  var $add_length = $("#add_length").val();
+  var s_banner = $('#success_banner')
+  // VALIDATE SONG INPUT
+  if($add_song === "" || $add_album === "" || $add_artist === ""){
+    return alert("All 3 field must have a value!");
+  }
+  else{
+    var userSong = [{
+      title: $add_song,
+      artist: $add_artist,
+      album: $add_album,
+      genre: $add_genre,
+      length: $add_length
+    }]
+    // ADDS ARRAY OBJECT TO SONG ARRAY
+    Songs.addSong(userSong[0])
+    // CLEARS INPUT FIELDS
+    clearPage();
+    // SHOWS WINNING BANNER
+    s_banner.fadeIn(500).removeClass('hidden').fadeOut(3000)
+  }
+    // PRINTS INPUT TO DOM
+    printStuff(userSong[0])
+    // ADDS ARTIST TO FILTER TAB
+    addArtistsIntoFilter(userSong)
+    // ADDS ALBUM TO FILTER TAB
+    addAlbumsIntoFilter(userSong)
+}
+
+function eventListeners(){
+  // ON SUBMIT CLICK, CALLS ADD USER INPUT SONG FUNCTION
+  var $add_song = $("#add");
+  $add_song.on("click", addUserInputSong)
+//CLEAR INPUT FIELDS ON ADD MUSIC PAGE
+  var clear_add_music_fields = $("#clear");
+  clear_add_music_fields.on("click", clearPage);
+// ADDS SECOND JSON FILE TO DOM ON 'MORE' CLICK
+  var $more = $('#add_more');
+  $more.on("click", function(){
+    Songs.loadFiles('lib/moreSongs.json')
+    $(this).css("display", "none");
+  })
+// TAKES RANGE INPUT AND CONVERTS TO TIME
+  var length = $('#length')
+  var l_val = $('#length_val')
+  length.on('input', function(){
+    var x = length.val()
+    l_val.html(convertTime(x))
+  })
+  //EXECUTES FILTER ON SONGS ARRAY WHEN FILTER BUTTON IS CLICKED
+  var filter = $('#filter')
+  filter.on('click', function(){
+    timeFilter(Songs.getSongs)
+    filterStuff(Songs.getSongs)
+  })
+}
+
+function buttonListeners(){
+  // REMOVES ELEMENT WHEN RED X IS CLICKED
+  var $song_element = $('.element')
+    $song_element.on("click", function(e){
+    if(e.target && e.target.nodeName === "BUTTON"){
+      $(this).remove('.element')
+    }
+  })
+
+    $song_element.on('mouseover', function(e){
+      if(e.target && e.target.nodeName === "BUTTON"){
+        $(e.target).animate('slow').removeClass('glyphicon glyphicon-remove delete').addClass('remove').html('Delete')
+      }
+    })
+
+    $song_element.on('mouseout', function(e){
+      if(e.target && e.target.nodeName === "BUTTON"){
+        $(e.target).addClass('glyphicon glyphicon-remove delete').removeClass('remove').html('')
+      }
+    })
+}
+// CONVERTS TO TIME FUNCTION
+function convertTime(time){
+  if(time.includes(':')){
+    return time
+  }
+  else{
+    var mins = Math.floor(time/60)
+    var secs = time % 60
+    var time = `${mins}:${(secs < 10 ? "0" : "")}${secs}`
+    return time
+  }
+}
---
+++ js/songs.js
@@ -0,0 +1,108 @@
+var Songs = (function(){
+  var _songs = [];
+
+  function addSongsToArray(item){
+    _songs[_songs.length] = item;
+  }
+
+  function showJSON(file){
+    $.ajax({
+      url: file
+    }).done(function(content){
+      addToDom(content)
+      spa()
+    })
+  }
+
+  function spa(){
+    $('#add_button').on("click", function(){
+      $("#add_music").removeClass('hidden');
+      $("#music").addClass("hidden");
+    })
+    $("#view_music").on("click", function(){
+      $("#music").removeClass('hidden');
+      $("#add_music").addClass('hidden');
+    })
+  }
+
+  return {
+    addSong: addSongsToArray,
+    getSongs: _songs,
+    loadFiles: showJSON
+  }
+})()
+
+function clearPage(){
+  $("#add_song").val("");
+  $("#add_album").val("");
+  $("#add_artist").val("");
+  $("#add_genre").val("");
+  $("#add_length").val("");
+}
+
+function addArtistsIntoFilter(arr){
+  var artist_select = document.getElementById('artist')
+  arr.filter(function(item){
+      return f_array = item.artist
+  })
+  artist_select.innerHTML += `<option value="${f_array}">${f_array}</option`
+  console.log(f_array)
+}
+
+function addAlbumsIntoFilter(arr){
+  var album_select = document.getElementById('album')
+  arr.filter(function(item){
+      return new_album = item.album
+  })
+  album_select.innerHTML += `<option value="${new_album}">${new_album}</option>`
+  console.log(new_album)
+}
+
+function printStuff(thing){
+  var songlist = $('#songlist')
+  var song = $("<div></div>")
+  song.addClass('element')
+  song.html(`<h2 class='song_name'>${thing.title}
+    <button class='delete glyphicon glyphicon-remove'></button></h2>
+    <p class='artist'>${thing.artist}</p><span> &nbsp;|&nbsp;</span>
+    <p class='album'>${thing.album}</p><span> &nbsp;|&nbsp; </span>
+    <p class="genre">${thing.genre}</p><span> &nbsp;|&nbsp; </span>
+    <p class="length">${convertTime(thing.length)}</p>`)
+  songlist.append(song)
+  buttonListeners()
+}
+
+function filterStuff(arr){
+  var artist = $('#artist').val()
+  var album = $('#album').val()
+  var genre = $('#genre').val()
+  $('#songlist').html("<div></div>")
+
+  arr.forEach(function(item){
+    if(artist === item.artist && artist !== 'all'){
+      printStuff(item)
+    }
+    else if(album === item.album && album !== 'all'){
+      printStuff(item)
+    }
+    else if(genre === item.genre && genre !== 'all'){
+      printStuff(item)
+    }
+    else if(artist === 'all' && album === 'all' && genre === 'all'){
+      printStuff(item)
+    }
+  })
+}
+
+function timeFilter(arr){
+  var length = $('#length').val()
+  $('#songlist').html(" ")
+  arr.forEach(function(item){
+    if(length > item.length){
+      printStuff(item)
+    }
+  })
+}
+
+Songs.loadFiles('lib/songs.json');
+eventListeners()
